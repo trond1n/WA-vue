ed31adaba89cb00979d4be1990c2fb30292f13a6
Yuri Trondin <manctzns@gmail.com>
2023-08-12 09:46:00 +0500

    компонент с погодой готов

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ const city = ref("Kyiv");
 const weatherInfo = ref(null);
 
 const getWeather = () => {
-  fetch(`${BASE_URL}?q=${city.value}&appid=${API_KEY}`)
+  fetch(`${BASE_URL}?q=${city.value}&units=metric&appid=${API_KEY}`)
     .then((res) => res.json())
     .then((data) => (weatherInfo.value = data));
 };
@@ -32,7 +32,7 @@ onMounted(getWeather());
                     @keyup.enter="getWeather"
                   />
                 </div>
-                <WeatherSummary />
+                <WeatherSummary :weatherInfo="weatherInfo" />
               </div>
             </section>
             <section class="section section-right">
